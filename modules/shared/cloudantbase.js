b28d1962a95eb232026588a6e1f179709bc06281
Nick Hester <nick_hester@hotmail.com>
2024-10-04 11:04:07 -0400

    Fixing wrong function name causing unhandled rejection on retry

--- modules/shared/cloudantbase.js
+++ modules/shared/cloudantbase.js
@@ -243,7 +243,7 @@ const insertDocumentWithRetry = (service, database, doc, attempts) => {
       .then((body) => resolve(body))
       .catch((err) => {
         if (attempts > 0) {
-          insertDoc(service, database, doc, attempts - 1)
+          insertDocument(service, database, doc, attempts - 1)
             .then((body) => resolve(body))
             .catch((err2) => reject(err2));
         } else {
@@ -289,7 +289,7 @@ const bulkDocumentsWithRetry = (service, database, docs, attempts) => {
       .then((body) => resolve(body))
       .catch((err) => {
         if (attempts > 0) {
-          bulkDocs(service, database, docs, attempts - 1)
+          bulkDocuments(service, database, docs, attempts - 1)
             .then((body) => resolve(body))
             .catch((err2) => reject(err2));
         } else {
